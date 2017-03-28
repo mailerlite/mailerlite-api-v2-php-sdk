e33d833fa707360556227065e86df184fd25b59b
Justinas Pošiūnas <justinas.posiunas@mailerlite.com>
2017-03-28 13:17:58 +0300

    http changed to https

--- src/Common/ApiConstants.php
+++ src/Common/ApiConstants.php
@@ -3,7 +3,7 @@
 namespace MailerLiteApi\Common;
 
 class ApiConstants {
-    const BASE_URL = "http://api.mailerlite.com/api/";
+    const BASE_URL = "https://api.mailerlite.com/api/";
     const VERSION = "v2";
     const SDK_USER_AGENT = "MailerLite PHP SDK";
     const SDK_VERSION = "2.0";
