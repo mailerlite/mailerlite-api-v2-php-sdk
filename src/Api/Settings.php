8cd9d5bc6a7e21b4268f2db61bb2c76f13ba7519
flowdee <ddjjmm@gmx.de>
2017-11-09 17:36:16 +0100

    added API settings endpoints

---
+++ src/Api/Settings.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MailerLiteApi\Api;
+
+use MailerLiteApi\Common\ApiAbstract;
+
+class Settings extends ApiAbstract {
+
+    protected $endpoint = 'settings';
+
+    /**
+     * Retrieve double opt in status
+     *
+     * @return mixed
+     */
+    public function getDoubleOptin()
+    {
+        $endpoint = $this->endpoint . '/double_optin';
+
+        $response = $this->restClient->get( $endpoint );
+
+        return $response['body'];
+    }
+
+    public function setDoubleOptin( $status ) {
+
+        $endpoint = $this->endpoint . '/double_optin';
+
+        $params = array_merge($this->prepareParams(), ['enable' => $status] );
+
+        $response = $this->restClient->post( $endpoint, $params );
+
+        return $response['body'];
+    }
+
+}
