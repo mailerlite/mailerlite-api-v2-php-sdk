0ff30d130c58051db3f6ed3dc20e82450535f03f
Nikol Petrunova <nikol@mailerlite.com>
2018-11-14 13:18:08 +0000

    remove dd

--- src/Api/WooCommerce.php
+++ src/Api/WooCommerce.php
@@ -21,7 +21,7 @@ class WooCommerce extends ApiAbstract
         $params = array_merge($this->prepareParams(), ['consumer_key' => $consumerKey, 'consumer_secret' => $consumerSecret, 'store' => $store, 'api_key' => $apiKey] );
 
         $response = $this->restClient->post( $endpoint, $params );
-        dd($response['body']);
+
         return $response['body'];
     }
 }
