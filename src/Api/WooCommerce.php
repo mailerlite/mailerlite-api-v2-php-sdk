5082b7c09e9f5a9351b8fd74d3078e2a147b9846
Nikol Petrunova <nikol@mailerlite.com>
2019-01-09 14:17:31 +0000

    passing shop currency to api

--- src/Api/WooCommerce.php
+++ src/Api/WooCommerce.php
@@ -9,21 +9,21 @@ class WooCommerce extends ApiAbstract
 {
     protected $endpoint = 'woocommerce';
 
-    public function setConsumerData( $consumerKey, $consumerSecret, $store, $apiKey )
+    public function setConsumerData( $consumerKey, $consumerSecret, $store, $apiKey, $currency )
     {
 
         $endpoint = $this->endpoint . '/consumer_data';
 
-        $params = array_merge($this->prepareParams(), ['consumer_key' => $consumerKey, 'consumer_secret' => $consumerSecret, 'store' => $store, 'api_key' => $apiKey] );
+        $params = array_merge($this->prepareParams(), ['consumer_key' => $consumerKey, 'consumer_secret' => $consumerSecret, 'store' => $store, 'api_key' => $apiKey, 'currency' => $currency] );
 
         $response = $this->restClient->post( $endpoint, $params );
         if (isset($response['body']->errors)) {
             return $response['body'];
         }
-        $webHookResponse = $this->setWebhooks($store);
+        /*$webHookResponse = $this->setWebhooks($store);
         if(isset($webHookResponse['body']) && isset($webHookResponse['body']->errors)) {
             return $webHookResponse['body'];
-        }
+        }*/
         
         return $response['body'];
     }
